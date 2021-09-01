791cb085cdc77cca947b1d13c916d0a25af5500d
MahfujSajibe <mahfujsajib1998@gmail.comm>
2021-09-02 04:13:39 +0600

    image add

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const searchBooks = () => {
     const bookName = document.getElementById('book-name');
     const searchBooks = bookName.value;
     bookName.value = ""
-    const url = `http://openlibrary.org/search.json?q=${searchBooks}`
+    const url = `http://openlibrary.org/search.json?q=${searchBooks}`;
     fetch(url)
         .then(res => res.json())
         .then(data => getBooks(data.docs))
@@ -10,6 +10,19 @@ const searchBooks = () => {
 const getBooks = getBook => {
     const searchBooks = document.getElementById('books');
     searchBooks.textContent = '';
+    // Show book Number
+    const bookNumber = document.getElementById('book-number');
+    bookNumber.textContent = "";
+    bookNumber.style.textAlign = "center";
+    bookNumber.style.color = "rgb(255, 0, 0)";
+    if (getBook.length > 0) {
+        const bookNumberDiv = document.createElement('div');
+        bookNumberDiv.innerHTML = `
+            <h3>Total Books: ${getBook.length}</h3>
+        `
+        bookNumber.appendChild(bookNumberDiv)
+    }
+    // Book Not Found
     const notFound = document.getElementById('not-found');
     notFound.style.textAlign = "center";
     notFound.style.color = "rgb(255, 0, 0)";
@@ -18,27 +31,28 @@ const getBooks = getBook => {
         notFoundDiv.innerHTML = `
             <h3>No result found</h3>
          `
-        notFound.appendChild(notFoundDiv)
+        notFound.appendChild(notFoundDiv);
     }
+    // Get your books
     getBook.forEach(book => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
             <div id = "books" class="card h-100" >
-                <img src="${book.last_modified_i} " class="card-img-top">
                 <div class="card-body">
-                        <h5 class="card-title">Title: ${book.title}</h5>
+                    <img class="img-fluid" src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" alt="">
+                        <h5 class="card-title mt-3">Book Name: ${book.title}</h5>
                         <p class="card-text">Author Name: ${book.author_name}</p>
                         <p>Publish Date: ${book.first_publish_year}</p>
                         <p>language: ${book.language}</p>
                         <p>Publisher: ${book.publisher}</p>
                         <p>Publis-Place: ${book.publish_place}</p>
 
-                 </div>
+                </div>
             </div>
 
         `
-        searchBooks.appendChild(div)
+        searchBooks.appendChild(div);
     });
 }
 
